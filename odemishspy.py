cb3f078114e55618cfee6730f28d84f47da294d6
Klaus Schwarzburg <kspublic@outlook.de>
2018-04-23 09:47:11 +0200

    xxx

--- odemishspy.py
+++ odemishspy.py
@@ -29,9 +29,14 @@ def odemis_to_hyperspy(filename='sampledata/cltest.h5',specbin=1) :
     elif 'CL intensity' in cdesc:
         cltype = 'panchrom'
 
-    print('<' + filename + '> original shape :' ,x.shape)
+    print('<' + filename + '> original shape :' ,x.shape, cltype)
+
     # strip unused dimensions and transpose/ reverse index order
-    xx=x[:,0,0,:,:].transpose((2,1,0))
+    if cltype == 'panchrom' :
+        xx=x[0,0,0,:,:].transpose((1,0))
+        # just an image..
+    else :
+        xx=x[:,0,0,:,:].transpose((2,1,0))
 
     if cltype == 'spectrum' :
         #interpolate data to linearize the wavelength scale
@@ -87,3 +92,50 @@ def odemis_to_hyperspy(filename='sampledata/cltest.h5',specbin=1) :
 
 
 #odemis_to_hyperspy()
+
+
+def odemisSEM_to_hyperspy(filename='sampledata/cltest.h5') :
+    """
+    odemis_to_hyperspy(filename='cltest.h5',specbin=1)
+    open h5 files from Delmic Odemis and convert them to a hyperspy object
+    reads in the spectral data (only) from hyperspectral maps and panchromatic
+    CL maps using the PM
+    1.arg: filename
+    2.arg: binning for hyperspectral maps
+
+    """
+
+    f=h5.File(filename,'r')
+    shome = 'Acquisition1//ImageData/'
+    x = f[shome + 'Image']
+    cdesc =f['Acquisition1/PhysicalData/ChannelDescription'].value[0].decode('utf-8')
+    #print(cdesc)
+
+
+    print('<' + filename + '> original shape :' ,x.shape)
+    # strip unused dimensions and transpose/ reverse index order
+    xx=x[0,0,0,:,:].transpose((1,0))
+
+    s = hs.signals.Signal2D(xx)
+    print('hyperspy shape :' ,s.data.shape)
+
+
+    s.metadata.General.title = 'Odemis: ' + cdesc
+    s.metadata.General.original_filename = filename
+    #s.metadata.General.notes = cltype
+    s.axes_manager[0].name = 'pos x'
+    s.axes_manager[0].scale = f[shome + 'DimensionScaleX'].value * 1e6
+    s.axes_manager[0].offset = f[shome + 'XOffset'].value * 1e6
+    s.axes_manager[0].units = 'um'
+
+
+    s.axes_manager[1].name = 'pos y'
+    s.axes_manager[1].scale = f[shome + 'DimensionScaleX'].value * 1e6
+    s.axes_manager[1].offset = f[shome + 'YOffset'].value * 1e6
+    s.axes_manager[1].units = 'um'
+
+
+    f.close()
+    return( s )
+    #end odemisSEM_to_hyperspy
+    #######################
